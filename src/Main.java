6ac963b89561e8b2c288b6468ef674c18e989f77
angel <angel.zazueta14@hotmail.com>
2023-08-26 07:03:14 -0700

    Listo para entregar :)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.io.*;
+import java.util.HashMap;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static final String SEPARADOR ="\\s+";   //Para separar las palabras por espacio :)
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new FileReader("src/divina_comedia_sp.txt"));
+        HashMap<Integer, Integer> histograma = new HashMap<>();
+        String line;
+        try {
+            while((line = br.readLine()) != null){
+                String [] palabras = line.split(SEPARADOR);
+                for(String palabra : palabras){
+                    palabra = palabra.replaceAll("[^a-zA-Z]", "" ); //Quitar caracteres que no son del alfabeto
+                    if(palabra.length() >=2 && palabra.length() <= 10 ){    //if que delimita la longitud de las palabras a considerar de 2 hasta 10 caracteres
+                    int length = palabra.length();
+                    histograma.put(length, histograma.getOrDefault(length, 0) + 1);
+                    }
+                }
+            } br.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Histograma de longitud de palabras en la divina comedia:");
+        for (int length = 2; length <= 10; length++) {
+            int contador = histograma.getOrDefault(length, 0);
+            System.out.printf("%d: %s%n", length, "[]".repeat(contador));   //imprime el histograma
+            System.out.println(contador +" palabras de " + length + " letras");
+        }
     }
 }
+
--- src/Matrices.java
+++ src/Matrices.java
@@ -1,2 +1,80 @@
-package PACKAGE_NAME;public class Matrices {
+import java.io.*;
+
+public class Matrices {
+    public static void main(String[] args) {
+
+        try{
+            FileInputStream FileaMat = new FileInputStream("src/a.mat");
+            FileInputStream FilebMat = new FileInputStream("src/b.mat");
+
+
+            DataInputStream aMat = new DataInputStream(FileaMat);
+            DataInputStream bMat = new DataInputStream(FilebMat);
+
+            int filasA = aMat.readByte();
+            int columnasA = aMat.readByte();
+            int filasB = bMat.readByte();
+            int columnasB = bMat.readByte();
+
+            if (columnasA != filasB){
+                System.out.println("Las matrices no se pueden multiplicar");
+                return;
+            }
+            double [][] matrizA = new double[filasA][columnasA];
+            double [][] matrizB = new double[filasB][columnasB];
+
+            for (int i = 0; i < filasA; i++) {
+                for (int j = 0; j < columnasA; j++) {
+                    matrizA [i][j] = aMat.readDouble();
+                }
+            }
+            for (int i = 0; i < filasB; i++) {
+                for (int j = 0; j < columnasB; j++) {
+                    matrizB [i][j] = bMat.readDouble();
+                }
+            }
+            aMat.close();
+            bMat.close();
+
+            double [][] matrizC = new double[filasA][columnasB];
+
+            for (int i = 0; i < filasA; i++) {
+                for (int j = 0; j < columnasB; j++) {
+                    double sum = 0;
+                    for (int k = 0; k < columnasA; k++) {
+                        sum += matrizA[i][k] * matrizB[k][j];
+                    }
+                    matrizC[i][j] = sum;
+                }
+            }
+            System.out.println("Matriz A: ");
+            printMatriz(matrizA);
+            System.out.println("Matriz B: ");
+            printMatriz(matrizB);
+            System.out.println("Matriz C: ");
+            printMatriz(matrizC);
+
+            DataOutputStream matrixCStream = new DataOutputStream(new FileOutputStream("c.mat"));
+            matrixCStream.writeByte(filasA);
+            matrixCStream.writeByte(columnasB);
+
+            for (int i = 0; i < filasA; i++) {
+                for (int j = 0; j < columnasB; j++) {
+                    matrixCStream.writeDouble(matrizC[i][j]);
+                }
+            }
+            System.out.println("Se ha guardado la matriz en c.mat");
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    public static void printMatriz(double[][] matriz) {
+        for (double[] row : matriz) {
+            for (double value : row) {
+                System.out.print(value + "\t");
+            }
+            System.out.println();
+        }
+    }
 }
